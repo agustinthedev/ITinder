ddb753b8fff51f74bfaf4b779135b14c57949bf6
Agustin Guarino <agustinguarino01@gmail.com>
2023-06-27 03:24:31 -0300

    Documented Moovx scraper code

--- Scrapers/Websites/Moovx.py
+++ Scrapers/Websites/Moovx.py
@@ -10,12 +10,13 @@ class Moovx:
     JOBS_FULL_DESC_BUTTON_ELEMENT_XPATH = "//div[@id='jobs']//div[@onmouseout=\"this.style.background='#FFFFFF'\"]//div[@class=\"col-3\"]//a" # XPATH for the position full description link
     JOB_FULL_DESCRIPTION_ELEMENT_XPATH = "//div[@id=\"job-details\"] | //div[@id=\"content\"]" # XPATH for full position description
 
-    driver = ""
-    jobs_list = []
-    locations_list = []
-    descriptions_links_list = []
-    full_descriptions_list = []
+    driver = "" # Chrome driver
+    jobs_list = [] # List of titles for open positions
+    locations_list = [] # List of locations available for the open positons (remote/on-site)
+    descriptions_links_list = [] # Link to the full description of the position
+    full_descriptions_list = [] # Full description of the positions
 
+    # Dictionary to replace words/phrases in the full position description
     replace_list = {"👨‍💻 Apply and start now!": "\n👨‍💻 Apply and start now!",
                     "Who we are": "\nWho we are:\n",
                     "What are you going to do?": "\nWhat are you going to do?\n",
@@ -42,18 +43,22 @@ class Moovx:
 
     def __init__(self):
         pass
-
+    
+    # Get listings from Moovx site
     def getListings(self):
+        # Open browser
         self.driver = webdriver.Chrome()
         self.driver.get(self.SITE_URL)
 
         sleep(3)
 
         try:
+            # Locate elements for jobs, locations and description links using XPATH
             jobs = self.driver.find_elements(By.XPATH, self.JOBS_TITLES_ELEMENT_XPATH)
             locations = self.driver.find_elements(By.XPATH, self.JOBS_LOCATION_ELEMENT_XPATH)
             descriptions_links = self.driver.find_elements(By.XPATH, self.JOBS_FULL_DESC_BUTTON_ELEMENT_XPATH)
 
+            # Scraping position titles
             for i in range(0, len(jobs)):
                 print(f"- Getting job title ({i+1}/{len(jobs)}).")
                 job = jobs[i]
@@ -61,9 +66,11 @@ class Moovx:
                 try:
                     self.jobs_list.append(job.text)
                 except Exception as e:
+                    # In case of exception, show error in console and add the value "Unknown" to the list as placeholder
                     print(f"Error while appending job title: {job} - Exception: {e}")
                     self.jobs_list.append("Unknown.")
 
+            # Scraping positions locations
             for i in range(0, len(locations)):
                 print(f"- Getting job location ({i+1}/{len(locations)}).")
                 location = locations[i]
@@ -71,8 +78,11 @@ class Moovx:
                 try:
                     self.locations_list.append(location.text)
                 except Exception as e:
+                    # In case of exception, show error in console and add the value "Unknown" to the list as placeholder
                     print(f"Error while appending location: {location} - Exception: {e}")
+                    self.locations_list.append("Unknown.")
 
+            # Scrape positions description links
             for i in range(0, len(descriptions_links)):
                 print(f"- Getting job description link: ({i+1}/{len(descriptions_links)}).")
                 description_link = descriptions_links[i]
@@ -80,16 +90,22 @@ class Moovx:
                 try:
                     self.descriptions_links_list.append(description_link.get_attribute('href').replace(";", ""))
                 except Exception as e:
+                    # In case of exception, show error in console and add the value "Unknown" to the list as placeholder
                     print(f"Error while appending listing url: {description_link} - Exception {e}")
+                    self.descriptions_links_list.append("Unknown.")
 
         except Exception as e:
+            # Throw error in case of general error
             print(f"Error while getting jobs listing: {e}")
         
+
+    # Get position detailed description from URL scraped before
     def getDetailedDescriptions(self):
         try:
             for i in range(0, len(self.descriptions_links_list)):
                 print(f"- Getting job detailed description ({i+1}/{len(self.descriptions_links_list)}).")
 
+                # Get link from list scraped before and navigate
                 desc_link = self.descriptions_links_list[i]
                 self.driver.get(desc_link)
 
@@ -98,28 +114,46 @@ class Moovx:
                 try:
                     full_description = self.driver.find_element(By.XPATH, self.JOB_FULL_DESCRIPTION_ELEMENT_XPATH).text
                 except Exception as e:
+                    # In case of exception, show error in console and add the value "Unknown" to the list as placeholder
                     print(f"Error while getting full description: {desc_link} - Exception: {e}")
                     full_description = "Unknown."
 
-                #full_description_parsed = self.parseDescription(full_description) # Parse description to replace specific strings
+                # Parse description to replace specific strings
                 for word, replacement in self.replace_list.items():
                     full_description = full_description.replace(word, replacement)
 
+                # Append description to list
                 self.full_descriptions_list.append(full_description)
 
+                '''
+
+                TO DO: REMOVE THIS WRITE TO FILE, ONLY USED TO DEBUG.
+
+                '''
                 with open(f"{i+1}.txt", "a", encoding="utf-8") as file:
                     file.write(full_description)
 
                 sleep(3)
         except Exception as e:
-            pass
+            # Throw error in case of general error
+            print(f"Error while getting jobs detailed description: {e}")
 
+    # Print all scraped listings (full description not included)
     def printListings(self):
         for i in range(0, len(self.jobs_list)):
             print(f"{self.jobs_list[i]} // {self.locations_list[i]} // {self.descriptions_links_list[i]}")
+
+    def generateOffers(self):
+        '''
+        
+        TO DO: Once all position listings are scraped, generate Offers and upload to site.
+
+        '''
     
 
-scraper = Moovx()
-scraper.getListings()
-scraper.printListings()
-scraper.getDetailedDescriptions()
+'''
+scraper = Moovx() # Initialize Class
+scraper.getListings() # Start scraping job listings and saving them to lists
+scraper.printListings() # Print all scraped listings (full description not included)
+scraper.getDetailedDescriptions() # Start getting detailed descriptions from URLs scraped before
+'''
