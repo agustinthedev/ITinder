2bd64d01d8aaba698d7d8619004b94516ac34ba2
Agustin Guarino <agustinguarino01@gmail.com>
2023-06-27 03:31:36 -0300

    Created initial Offers structure and moved Offers to Website folder

--- Scrapers/Websites/Moovx.py
+++ Scrapers/Websites/Moovx.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+import Offers
 
 class Moovx:
 
---
+++ Scrapers/Websites/Offers.py
@@ -0,0 +1,20 @@
+class Offers:
+    OFFER_COMPANY = ""
+    OFFER_TITLE = ""
+    OFFER_LOCATION = ""
+    OFFER_LINK = ""
+    OFFER_DESCRIPTION = ""
+
+    def __init__(self, company, title, location, link, description):
+        self.OFFER_COMPANY = company
+        self.OFFER_TITLE = title
+        self.OFFER_LOCATION = location
+        self.OFFER_LINK = link
+        self.OFFER_DESCRIPTION = description
+
+    def loadOffer(self):
+        '''
+        
+        TO DO: SAVE OFFER TO DB.
+
+        '''
