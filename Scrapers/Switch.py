326b34b58dbd3a248909bf9046f1c4f7375f9356
Agustin Guarino <agustinguarino01@gmail.com>
2023-06-29 05:23:23 -0300

    Basic structure for Switch scraper

---
+++ Scrapers/Switch.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+from Offers import Offer
+
+class Switch:
+
+    SITE_URL = "https://switch.hiringroom.com/jobs"
+    SHOW_ALL_POSITIONS_BUTTON_XPATH = ""
+    JOBS_TITLES_ELEMENT_XPATH = ""
+    JOBS_LOCATION_ELEMENT_XPATH = ""
+    JOBS_FULL_DESCRIPTION_LINK_XPATH = ""
+    JOB_FULL_DESCRIPTION_ELEMENT_XPATH = ""
+
+    driver = ""
+    jobs_list = []
+    locations_list = []
+    descriptions_links_list = []
+    full_descriptions_list = []
+
+    replace_list = {}
+
+    def __init__(self):
+        pass
+
+    def getListings():
+        pass
+
+    def getDetailedDescriptions(self):
+        pass
+
+    def generateOffers(self):
+        pass
+
+    def start(self):
+        self.getListings()
+        self.getDetailedDescriptions()
+        self.generateOffers
