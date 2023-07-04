8064ad829612bc96d7bd0cfef4d32a9054d44b87
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-04 18:57:52 -0300

    Basic full description scraper for FullStackLabs

--- Scrapers/Websites/FullStackLabs.py
+++ Scrapers/Websites/FullStackLabs.py
@@ -10,7 +10,7 @@ class FullStackLabs:
     JOBS_TITLES_ELEMENT_XPATH = "//a[@class='posting-title']//h5[@data-qa='posting-name']"
     JOBS_LOCATION_ELEMENT_XPATH = ""
     JOBS_FULL_DESC_BUTTON_ELEMENT_XPATH = "//a[@class='posting-title']"
-    JOB_FULL_DESCRIPTION_ELEMENT_XPATH = ""
+    JOB_FULL_DESCRIPTION_ELEMENT_XPATH = "//div[@class=\"section-wrapper page-full-width\"]"
 
     driver = ""
     jobs_list = []
@@ -18,7 +18,16 @@ class FullStackLabs:
     descriptions_links_list = []
     full_descriptions_list = []
 
-    replace_list = {}
+    replace_list = {"We’re most proud of:": "We’re most proud of:\n",
+                    "The Position:": "\nThe Position:\n",
+                    "What We're Looking For:": "\nWhat We're Looking For:\n",
+                    "Our Benefits:": "\nOur Benefits:\n",
+                    "You'll work with our incredible clients in one of two ways:": "You'll work with our incredible clients in one of two ways:\n",
+                    "What we are looking for:": "\nWhat we are looking for:\n",
+                    "Benefits:": "\nBenefits:\n",
+                    "Learn more about our Applicants Privacy Notice.": "",
+                    "\n\nAPPLY FOR THIS JOB": "",
+                    "FullStack is proud to be an equal opportunity workplace. We are committed to equal employment opportunity regardless of race, color, ancestry, religion, sex, national origin, sexual orientation, age, citizenship, marital status, disability, gender identity, or Veteran status. If you have a disability or special need that requires accommodation, please let us know by completing our Accommodations for Applicants form, which can be provided upon request during our hiring and interview process.": ""}
 
     def __init__(self):
         pass
@@ -53,20 +62,38 @@ class FullStackLabs:
                         self.locations_list.append("Remote")
                         self.descriptions_links_list.append(temp_desc_links[i])
 
-                        print(f"{temp_titles[i].replace('- Remote ', '')} // {temp_desc_links[i]}")
-
-            print(f"Total positions: {len(self.jobs_list)} // Total description links: {len(self.descriptions_links_list)}")
-
         except Exception as e:
             print(e)
 
-        
-
-
-
-
     def getDetailedDescriptions(self):
-        pass
+        try:
+            for i in range(0, len(self.descriptions_links_list)):
+                print(f"- Getting job detailed description ({i+1}/{len(self.descriptions_links_list)}).")
+
+                desc_link = self.descriptions_links_list[i]
+                self.driver.get(desc_link)
+                
+                sleep(2)
+
+                try:
+                    full_description = self.driver.find_element(By.XPATH, self.JOB_FULL_DESCRIPTION_ELEMENT_XPATH).text
+                except Exception as e:
+                    print(f"Error while getting full description: {desc_link} - Exception: {e}")
+                    full_description = "Unknown."
+
+                for word, replacement in self.replace_list.items():
+                    full_description = full_description.replace(word, replacement).strip()
+                    print(full_description)
+
+                self.full_descriptions_list.append(full_description)
+
+                with open(f"{i+1}.txt", "a", encoding="utf-8") as file:
+                    file.write(full_description)
+
+                sleep(3)
+
+        except Exception as e:
+            print(e)
 
     def generateOffers(self):
         pass
@@ -76,3 +103,4 @@ class FullStackLabs:
 
 scraper = FullStackLabs()
 scraper.getListings()
+scraper.getDetailedDescriptions()
