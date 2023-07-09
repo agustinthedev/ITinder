837c67b15f58b9f531f9e45e1206b57b6f5f01ed
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-09 01:08:15 -0300

    Basic function for DB file

--- Scrapers/Websites/Util/DB.py
+++ Scrapers/Websites/Util/DB.py
@@ -20,10 +20,18 @@ class Database:
         self.cursor = self.db.cursor()
 
     def addListing(self, company, title, location, link, description):
-        query = "INSERT INTO job_listings (id, company, title, location, link, description) VALUES (NULL, %s, %s, %s, %s, %s)"
-        values = (company, title, location, link, description)
+        #query = "INSERT INTO job_listings (id, company, title, location, link, description) VALUES (NULL, %s, %s, %s, %s, %s)"
+        #values = (company, title, location, link, description)
 
-        self.cursor.execute(query, values)
-        self.db.commit()
+        query = "SELECT id FROM job_listings WHERE link = 'https://jobs.lever.co/fullstacklabs/ac6e1b5f-2ad5-4564-baac-cf1489a1f23d'"
 
-        print("Inserted new job listing into 'job_listings' table.")
+        self.cursor.execute(query)
+
+        for x in self.cursor:
+            print(x)
+        #self.db.commit()
+
+        #print("Inserted new job listing into 'job_listings' table.")
+
+db = Database()
+db.addListing("Google", "QA", "Remote", "https://jobs.lever.co/fullstacklabs/ac6e1b5f-2ad5-4564-baac-cf1489a1f23d", "test")
