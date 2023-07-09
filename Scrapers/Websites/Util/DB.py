459be3d63f50a76dcb798d7682f8435c899b61bf
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-08 21:03:32 -0300

    Basic strcuture for DB file

---
+++ Scrapers/Websites/Util/DB.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv, find_dotenv
+import os
+import mysql.connector
+
+class Database:
+    
+    def __init__(self):
+        pass
+
+    def addListing(self, company, title, location, link, description):
+        pass
