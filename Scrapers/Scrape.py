eeb5e6fd79551fbb3e6741ba57e921aab4ad5b36
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-09 01:57:38 -0300

    Basic function for saveData function

--- Scrapers/Scrape.py
+++ Scrapers/Scrape.py
@@ -19,7 +19,18 @@ class Scraper:
             scraper.startScraping()
             data = scraper.getData()
             self.job_data.append(data)
-            print(self.job_data)
+
+    def saveData(self):
+        for i in range(len(self.job_data)):
+            company = self.job_data[i]["company"]
+            job_list = self.job_data[i]["job_list"]
+            location_list = self.job_data[i]["location_list"]
+            link_list = self.job_data[i]["link_list"]
+            full_descriptions = self.job_data[i]["full_descriptions"]
+
+            print(f"{company} \n {job_list} \n {location_list} \n {link_list}")
+
 
 scraper = Scraper()
 scraper.startScrapers()
+scraper.saveData()
