a35be72c5c95f70a98c2c3cbf3a9b16571983eae
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-08 23:05:03 -0300

    Basic structure in DB.py file to insert job listings into DB

--- Scrapers/Websites/Util/DB.py
+++ Scrapers/Websites/Util/DB.py
@@ -3,9 +3,30 @@ import os
 import mysql.connector
 
 class Database:
-    
+
+    db = ""
+    cursor = ""
+
     def __init__(self):
-        pass
+        load_dotenv(find_dotenv())
+        self.db = mysql.connector.connect(
+            host = os.getenv("DB_HOST"),
+            port = os.getenv("DB_PORT"),
+            user = os.getenv("DB_USER"),
+            passwd = os.getenv("DB_PASSWD"),
+            database = os.getenv("DB_DATABASE")
+        )
+
+        self.cursor = self.db.cursor()
 
     def addListing(self, company, title, location, link, description):
-        pass
+        query = "INSERT INTO job_listings (id, company, title, location, link, description) VALUES (NULL, %s, %s, %s, %s, %s)"
+        values = (company, title, location, link, description)
+
+        self.cursor.execute(query, values)
+        self.db.commit()
+
+        print("Inserted new job listing into 'job_listings' table.")
+
+database = Database()
+database.addListing("Google", "QA Engineer 4", "Remote", "https://careers.google.com/some-test-example", "This is a description example for a well known position in the IT industry.")
